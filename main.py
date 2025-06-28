9223b308a4d1c75f3058ce895189d71c816869ab
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2025-06-28 07:44:57 +0100

    A: first commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+
+def main():
+    load_dotenv()
+
+    args = sys.argv[1:]
+
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+    
+    verbose = "--verbose" in args
+    
+    user_prompt = args[0]
+    
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    client = genai.Client(api_key=api_key)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+    
+    generate_content(client, messages, verbose)
+
+
+def generate_content(client, messages, verbose):
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+
+    parameters = response.usage_metadata
+
+    if verbose:
+        print(f"User prompt: {messages[0].parts[0].text}")
+        print(f"Prompt tokens: {parameters.prompt_token_count}")
+        print(f"Response tokens: {parameters.candidates_token_count}")
+
+
+if __name__ == "__main__":
+    main()
