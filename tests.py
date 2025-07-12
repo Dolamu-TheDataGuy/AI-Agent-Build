22b0d5a21d913ad5454c2c1e443e7ee96bb8fa8a
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2025-07-12 08:05:31 +0100

    C: Add testscript for get_files_info function

---
+++ tests.py
@@ -0,0 +1,18 @@
+from functions.get_files_info import get_files_info
+
+if __name__ == "__main__":
+    print("Result for current directory:")
+    result1 = get_files_info("calculator", ".")
+    print(result1)
+
+    print("\nResult for 'pkg' directory:")
+    result2 = get_files_info("calculator", "pkg")
+    print(result2)
+
+    print("\nResult for '/bin' directory:")
+    result3 = get_files_info("calculator", "/bin")
+    print(result3)
+
+    print("\nResult for '../' directory:")
+    result3 = get_files_info("calculator", "../")
+    print(result3)
