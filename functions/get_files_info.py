b6ec12caa229b88b19f36aaa2714eecbf7118c43
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2025-07-12 08:04:25 +0100

    B: Add function to get content of directory

---
+++ functions/get_files_info.py
@@ -0,0 +1,27 @@
+import os
+
+def get_files_info(working_directory, directory=None):
+    if directory is None:
+        directory = '.'
+        
+    working_dir = os.path.abspath(working_directory)
+    target_dir = os.path.abspath(os.path.join(working_directory, directory))
+        
+
+    if not target_dir.startswith(working_dir):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+    if not os.path.isdir(target_dir):
+        return f'Error: "{directory}" is not a directory'
+        
+    try:    
+        full_path_contents = os.listdir(target_dir)
+
+        str = ""
+        for content in full_path_contents:
+            str += f"- {content}: file_size={os.path.getsize(os.path.join(target_dir,content))} bytes, is_dir={os.path.isdir(os.path.join(target_dir,content))}\n"
+        return str.strip()
+    
+    except Exception as e:
+        return f"Error: {str(e)}"
+
