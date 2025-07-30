99d00544c382b52ab10354861cb464eebec729f6
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2025-07-30 02:32:47 +0100

    G: Optimize readability of code by avoiding duplicated logic

--- functions/run_python.py
+++ functions/run_python.py
@@ -15,19 +15,23 @@ def run_python_file(working_directory, file_path, args=[]):
         f'Error: "{file_path}" is not a Python file.'
 
     try:
-        if args:
-            result = subprocess.run(["python3", abs_file_path, *args], check=True, timeout=30, cwd=abs_working_dir)
-        else:
-            result = subprocess.run(["python3", abs_file_path], check=True, timeout=30, cwd=abs_working_dir)
+        # if args:
+        #     result = subprocess.run(["python3", abs_file_path, *args], check=True, timeout=30, cwd=abs_working_dir)
+        # else:
+        #     result = subprocess.run(["python3", abs_file_path], check=True, timeout=30, cwd=abs_working_dir)
+
+        command = ["python3", abs_file_path, *args] if args else ["python3", abs_file_path]
         
+        result = subprocess.run(command, check=True, timeout=30, cwd=abs_working_dir)
+
         print(f"STDOUT = {result.stdout}")
         print(f"STDERR = {result.stderr}")
-        
+
         if result.returncode != 0:
             return f"Error: Python script exited with code {result.returncode}"
-        
+
         if not result.stdout:
             return "No output produced"
-            
+
     except Exception as e:
         return f"Error: {str(e)}"
